79dd41fb5ebe5c5d7f24150ccac44df1f36d87d1
Nikhit Kumar <nikhitkumar00@gmail.com>
2023-11-27 16:21:55 +0530

    done

---
+++ Lex Compiler using lex/input.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+    int a = 10;
+    char str[10];
+    printf("%d", a);
+    scanf("%s", str);
+    if (a == 10)
+    {
+        printf("%d", a);
+    }
+}
