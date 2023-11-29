2df6fd7e193d1a4a4829e448642b2a5bf795d658
Nikhit Kumar <nikhitkumar00@gmail.com>
2023-11-29 07:21:29 +0530

    done

--- Recursive Descent Parsing/program.c
+++ Recursive Descent Parsing/program.c
@@ -4,31 +4,29 @@
 #define SUCCESS 1
 #define FAILED 0
 
-// Function prototypes
-int E(), Edash(), T(), Tdash(), F();
-
 const char *cursor;
 char string[64];
+
+// Function prototypes
+int E(), Edash(), T(), Tdash(), F();
 
 int main()
 {
     puts("Enter the string");
-    scanf("%s", string); // Read input from the user
+    scanf("%s", string);
     cursor = string;
-    puts("");
-    puts("Input		 Action");
-    puts("--------------------------------");
+    puts("\nInput         Action");
+    puts("-----------------------");
 
-    // Call the starting non-terminal E
     if (E() && *cursor == '\0')
-    { // If parsing is successful and the cursor has reached the end
-        puts("--------------------------------");
+    {
+        puts("-----------------------");
         puts("String is successfully parsed");
         return 0;
     }
     else
     {
-        puts("--------------------------------");
+        puts("-----------------------");
         puts("Error in parsing String");
         return 1;
     }
@@ -37,16 +35,8 @@ int main()
 // Grammar rule: E -> T E'
 int E()
 {
-    printf("%-16s E -> T E'\n", cursor);
-    if (T())
-    {                // Call non-terminal T
-        if (Edash()) // Call non-terminal E'
-            return SUCCESS;
-        else
-            return FAILED;
-    }
-    else
-        return FAILED;
+    printf("%-12s E -> T E'\n", cursor);
+    return T() && Edash();
 }
 
 // Grammar rule: E' -> + T E' | $
@@ -54,21 +44,13 @@ int Edash()
 {
     if (*cursor == '+')
     {
-        printf("%-16s E' -> + T E'\n", cursor);
+        printf("%-12s E' -> + T E'\n", cursor);
         cursor++;
-        if (T())
-        {                // Call non-terminal T
-            if (Edash()) // Call non-terminal E'
-                return SUCCESS;
-            else
-                return FAILED;
-        }
-        else
-            return FAILED;
+        return T() && Edash();
     }
     else
     {
-        printf("%-16s E' -> $\n", cursor);
+        printf("%-12s E' -> $\n", cursor);
         return SUCCESS;
     }
 }
@@ -76,16 +58,8 @@ int Edash()
 // Grammar rule: T -> F T'
 int T()
 {
-    printf("%-16s T -> F T'\n", cursor);
-    if (F())
-    {                // Call non-terminal F
-        if (Tdash()) // Call non-terminal T'
-            return SUCCESS;
-        else
-            return FAILED;
-    }
-    else
-        return FAILED;
+    printf("%-12s T -> F T'\n", cursor);
+    return F() && Tdash();
 }
 
 // Grammar rule: T' -> * F T' | $
@@ -93,21 +67,13 @@ int Tdash()
 {
     if (*cursor == '*')
     {
-        printf("%-16s T' -> * F T'\n", cursor);
+        printf("%-12s T' -> * F T'\n", cursor);
         cursor++;
-        if (F())
-        {                // Call non-terminal F
-            if (Tdash()) // Call non-terminal T'
-                return SUCCESS;
-            else
-                return FAILED;
-        }
-        else
-            return FAILED;
+        return F() && Tdash();
     }
     else
     {
-        printf("%-16s T' -> $\n", cursor);
+        printf("%-12s T' -> $\n", cursor);
         return SUCCESS;
     }
 }
@@ -117,27 +83,18 @@ int F()
 {
     if (*cursor == '(')
     {
-        printf("%-16s F -> ( E )\n", cursor);
+        printf("%-12s F -> ( E )\n", cursor);
         cursor++;
-        if (E())
-        { // Call non-terminal E
-            if (*cursor == ')')
-            {
-                cursor++;
-                return SUCCESS;
-            }
-            else
-                return FAILED;
-        }
-        else
-            return FAILED;
+        return E() && (*cursor == ')' ? cursor++ && SUCCESS : FAILED);
     }
     else if (*cursor == 'i')
     {
-        printf("%-16s F -> i\n", cursor);
+        printf("%-12s F -> i\n", cursor);
         cursor++;
         return SUCCESS;
     }
     else
+    {
         return FAILED;
+    }
 }
