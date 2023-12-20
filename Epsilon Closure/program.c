c0d94956b8c2dd8fed58cd2a19e693695b43abe5
Nikhit Kumar <nikhitkumar00@gmail.com>
2023-12-20 14:55:40 +0530

    updated easier code

--- Epsilon Closure/program.c
+++ Epsilon Closure/program.c
@@ -1,63 +1,65 @@
 #include <stdio.h>
 #include <string.h>
 
-char result[20][20], copy[3], states[20][20];
-
-void add_state(char a[3], int i)
+struct node
 {
-    strcpy(result[i], a);
-}
+    char state1;
+    char transition;
+    char state2;
+} nodes[10];
 
-void display(int n)
-{
-    int k = 0;
-    printf("\nEpsilon closure of %s = {", copy);
-    while (k < n)
-    {
-        printf(" %s", result[k]);
-        k++;
-    }
-    printf(" } \n");
-}
+char states[5] = {'A', 'B', 'C', 'D'};
 
-int main()
+void findClosure(char a, int flag)
 {
-    FILE *INPUT;
-    INPUT = fopen("input.dat", "r");
-    char state[3];
-    int end, i = 0, n, k = 0;
-    char state1[3], input[3], state2[3];
-    printf("Enter the no. of states: ");
-    scanf("%d", &n);
-    printf("Enter the states: ");
-    for (k = 0; k < n; k++)
+    char result[10];
+    if (flag == 0)
+        printf("%c ", a);
+
+    for (int j = 0; j < 6; j++)
     {
-        scanf("%s", states[k]);
-    }
-    for (k = 0; k < n; k++)
-    {
-        i = 0;
-        strcpy(state, states[k]);
-        strcpy(copy, state);
-        add_state(state, i++);
-        while (1)
+        if (nodes[j].state1 == a && nodes[j].transition == 'e')
         {
-            end = fscanf(INPUT, "%s%s%s", state1, input, state2);
-            if (end == EOF)
-            {
-                break;
-            }
-            if (strcmp(state, state1) == 0)
-            {
-                if (strcmp(input, "e") == 0)
-                {
-                    add_state(state2, i++);
-                    strcpy(state, state2);
-                }
-            }
+            printf("%c ", nodes[j].state2);
+            findClosure(nodes[j].state2, 1);
         }
-        display(i);
-        rewind(INPUT);
     }
-    return 0;
+}
+
+void main()
+{
+    printf("THE EPSILON CLOSURES ARE :\n\n");
+    nodes[0].state1 = 'A';
+    nodes[0].transition = '0';
+    nodes[0].state2 = 'A';
+
+    nodes[1].state1 = 'A';
+    nodes[1].transition = 'e';
+    nodes[1].state2 = 'B';
+
+    nodes[2].state1 = 'B';
+    nodes[2].transition = '0';
+    nodes[2].state2 = 'C';
+
+    nodes[3].state1 = 'B';
+    nodes[3].transition = 'e';
+    nodes[3].state2 = 'D';
+
+    nodes[4].state1 = 'C';
+    nodes[4].transition = '1';
+    nodes[4].state2 = 'B';
+
+    nodes[5].state1 = 'D';
+    nodes[5].transition = '0';
+    nodes[5].state2 = 'D';
+
+    nodes[6].state1 = 'D';
+    nodes[6].transition = '1';
+    nodes[6].state2 = 'D';
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("\n\n%c -> ", states[i]);
+        findClosure(states[i], 0);
+    }
 }
